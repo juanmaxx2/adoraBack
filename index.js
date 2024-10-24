77bcbed0a759a36d8553c9519384a94a6f5dea4f
juanma <juanmartinlopez@saintpaul.edu.ar>
2024-10-24 19:02:52 -0300

    Quiero hacer el deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const institutionLoad = require("./src/helpers/institutionLoad.js");
 const provinceLoad = require("./src/helpers/provinceLoad.js");
 const { cloudinary_js_config } = require("./src/helpers/cloudinary.js");
 
-conn.sync({ force: false }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   await provinceLoad();
   await institutionLoad();
   cloudinary_js_config;
